3a2f7342e9de1956ff778c303bff1aafcada6163
GGLuks <64063689+GGLuks@users.noreply.github.com>
2020-12-02 08:31:57 +0700

    Merge branch 'master' of C:\Users\berna\P3B\tubes-02\Tubes02-A with conflicts.

--- app/src/main/java/com/example/tubes02_a/View/ThreadHandler.java
+++ app/src/main/java/com/example/tubes02_a/View/ThreadHandler.java
@@ -1,56 +1,30 @@
 package com.example.tubes02_a.View;
 
-import android.graphics.Color;
 import android.graphics.PointF;
-import android.graphics.Rect;
-import android.graphics.RectF;
 import android.os.Handler;
 import android.os.Message;
-import android.util.Log;
 
-import androidx.core.content.res.ResourcesCompat;
-
-import com.example.tubes02_a.R;
 
 public class ThreadHandler extends Handler {
+    protected MainActivity mainActivity;
+    protected final static int DRAW_RECT = 1;
 
-    protected GameFragment gameFragment;
-    String left = "1";
-    String right = "1";
-
-    public ThreadHandler(GameFragment gameFragment){
-        this.gameFragment = gameFragment;
+    public ThreadHandler(MainActivity mainActivity) {
+        this.mainActivity = mainActivity;
     }
 
-    @Override
-    public void handleMessage( Message msg){
-        if (msg.what == 0){
-            left = (String)msg.obj;
-        }
-        if (msg.what == 1){
-            right = (String)msg.obj;
-        }
+    public void handleMessage(Message msg){
+        if(msg.what==ThreadHandler.DRAW_RECT){
 
-
-        float leftPost = Float.valueOf(left), rightPost = Float.valueOf(right);
-        float topPost = 0, bottomPost = this.gameFragment.ivCanvas.getHeight() / 4;
-        this.gameFragment.mCanvas.drawColor(Color.parseColor("#a6fff6"));
-        this.gameFragment.mCanvas.drawRect(leftPost,topPost, rightPost,bottomPost, this.gameFragment.paint);
-        this.gameFragment.ivCanvas.invalidate();
+        }
     }
-
-    public void setLeft(String output){
+    public void drawRect(PointF pointF,boolean isClicked){
+        if(isClicked){
+            return;
+        }
         Message msg = new Message();
-        msg.what = 0;
-        msg.obj = output;
+        msg.what = ThreadHandler.DRAW_RECT;
+        msg.obj = pointF;
         this.sendMessage(msg);
     }
-
-     public void setRight(String output){
-        Message msg = new Message();
-        msg.what = 1;
-        msg.obj = output;
-        this.sendMessage(msg);
-    }
-
 }
