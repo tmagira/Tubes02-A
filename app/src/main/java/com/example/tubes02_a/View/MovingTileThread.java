61eb590d01f7b35242c813d6e381748875e97b7c
GGLuks <64063689+GGLuks@users.noreply.github.com>
2020-12-03 13:26:30 +0700

    moving tile

---
+++ app/src/main/java/com/example/tubes02_a/View/MovingTileThread.java
@@ -0,0 +1,56 @@
+package com.example.tubes02_a.View;
+
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MovingTileThread implements Runnable {
+
+    protected Thread thread;
+    protected ThreadHandler threadHandler;
+    protected GameFragment gameFragment;
+    float widthTile = 0, heightTile = 0, startX = 0;
+    boolean check = true;
+
+
+    public MovingTileThread (ThreadHandler threadHandler, GameFragment gameFragment){
+        this.thread = new Thread(this);
+        this.gameFragment = gameFragment;
+        this.threadHandler = threadHandler;
+    }
+
+    public void startThread(){
+        this.thread.start();
+    }
+
+    private void refresh() {
+        threadHandler.postDelayed(this,500);
+    }
+
+    @Override
+    public void run() {
+
+        float kiri, kanan;
+
+        if ( check == true){
+            widthTile = this.gameFragment.screenX() / 4;
+            heightTile = this.gameFragment.screenY()/ 4;
+            check = false;
+        }
+
+        Random random = new Random();
+
+        int columnATM = random.nextInt(5 - 1) + 1;
+
+//        Log.d("test", Integer.toString(columnATM));
+
+        kiri = startX + (( (float) columnATM - 1) * widthTile);
+        kanan = kiri + widthTile;
+
+        threadHandler.setLeft(String.valueOf(kiri));
+        threadHandler.setRight(String.valueOf(kanan));
+
+        refresh();
+    }
+}
--- app/src/main/java/com/example/tubes02_a/View/ThreadHandler.java
+++ app/src/main/java/com/example/tubes02_a/View/ThreadHandler.java
@@ -1,15 +1,28 @@
 package com.example.tubes02_a.View;
 
+<<<<<<< Updated upstream
+=======
+import android.graphics.Color;
+>>>>>>> Stashed changes
 import android.graphics.PointF;
 import android.os.Handler;
 import android.os.Message;
+<<<<<<< Updated upstream
 
 import com.example.tubes02_a.MainActivity;
+=======
+import android.util.Log;
+
+import androidx.core.content.res.ResourcesCompat;
+
+import com.example.tubes02_a.R;
+>>>>>>> Stashed changes
 
 public class ThreadHandler extends Handler {
     protected MainActivity mainActivity;
     protected final static int DRAW_RECT = 1;
 
+<<<<<<< Updated upstream
     public ThreadHandler(MainActivity mainActivity) {
         this.mainActivity = mainActivity;
     }
@@ -23,9 +36,48 @@ public class ThreadHandler extends Handler {
         if(isClicked){
             return;
         }
+=======
+    protected GameFragment gameFragment;
+    String left = "1";
+    String right = "1";
+
+    public ThreadHandler(GameFragment gameFragment){
+        this.gameFragment = gameFragment;
+    }
+
+    @Override
+    public void handleMessage( Message msg){
+        if (msg.what == 0){
+            left = (String)msg.obj;
+        }
+        if (msg.what == 1){
+            right = (String)msg.obj;
+        }
+
+
+        float leftPost = Float.valueOf(left), rightPost = Float.valueOf(right);
+        float topPost = 0, bottomPost = this.gameFragment.ivCanvas.getHeight() / 4;
+        this.gameFragment.mCanvas.drawColor(Color.parseColor("#a6fff6"));
+        this.gameFragment.mCanvas.drawRect(leftPost,topPost, rightPost,bottomPost, this.gameFragment.paint);
+        this.gameFragment.ivCanvas.invalidate();
+    }
+
+    public void setLeft(String output){
+        Message msg = new Message();
+        msg.what = 0;
+        msg.obj = output;
+        this.sendMessage(msg);
+    }
+
+     public void setRight(String output){
+>>>>>>> Stashed changes
         Message msg = new Message();
         msg.what = ThreadHandler.DRAW_RECT;
         msg.obj = pointF;
         this.sendMessage(msg);
     }
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 }
