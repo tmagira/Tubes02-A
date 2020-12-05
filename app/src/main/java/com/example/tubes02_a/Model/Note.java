2ca0e07093435c50958e78d143d7994762289c76
Clement Rivandi <6181801030@student.unpar.ac.id>
2020-12-05 15:40:53 +0700

    model

--- app/src/main/java/com/example/tubes02_a/Model/Note.java
+++
@@ -1,4 +0,0 @@
-package com.example.tubes02_a.Model;
-
-public class Note {
-}
--- app/src/main/java/com/example/tubes02_a/Model/Tile.java
+++ app/src/main/java/com/example/tubes02_a/Model/Tile.java
@@ -1,7 +1,47 @@
 package com.example.tubes02_a.Model;
 
 public class Tile {
-    int positionX;
-    int positionY;
+    float left;
+    float top;
+    float right;
+    float bottom;
 
+    public float getLeft() {
+        return left;
+    }
+
+    public void setLeft(float left) {
+        this.left = left;
+    }
+
+    public float getTop() {
+        return top;
+    }
+
+    public void setTop(float top) {
+        this.top = top;
+    }
+
+    public float getRight() {
+        return right;
+    }
+
+    public void setRight(float right) {
+        this.right = right;
+    }
+
+    public float getBottom() {
+        return bottom;
+    }
+
+    public void setBottom(float bottom) {
+        this.bottom = bottom;
+    }
+
+    public Tile(float left, float top, float right, float bottom) {
+        this.left = left;
+        this.top = top;
+        this.right = right;
+        this.bottom = bottom;
+    }
 }
